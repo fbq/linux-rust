b02e685de36ee7cb4f09cfde222168941d670c1b
Boqun Feng <boqun.feng@gmail.com>
2023-02-21 20:25:56 -0800

    notime like now ;-)
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- rust/kernel/time.rs
+++ rust/kernel/time.rs
@@ -6,20 +6,66 @@
 //! C header: [`include/linux/timekeeping.h`](../../../../include/linux/timekeeping.h)
 
 use crate::bindings;
-use core::time::Duration;
+use core::marker::PhantomData;
+// Re-exports [`Duration`], so that it's easy to provide kernel's own implemention in the future.
+pub use core::time::Duration;
 
-/// Returns the kernel time elapsed since boot, excluding time spent sleeping, as a [`Duration`].
-pub fn ktime_get() -> Duration {
-    // SAFETY: Function has no side effects and no inputs.
-    Duration::from_nanos(unsafe { bindings::ktime_get() }.try_into().unwrap())
+/// Provides a `now` function to get the current timestamp.
+pub trait TimeNow: Sized + Copy {
+    /// Returns the current timestamp.
+    fn now() -> Instant<Self>;
 }
 
-/// Returns the kernel time elapsed since boot, including time spent sleeping, as a [`Duration`].
-pub fn ktime_get_boottime() -> Duration {
-    Duration::from_nanos(
-        // SAFETY: Function has no side effects and no variable inputs.
-        unsafe { bindings::ktime_get_with_offset(bindings::tk_offsets_TK_OFFS_BOOT) }
-            .try_into()
-            .unwrap(),
-    )
+/// A timestamp of monotonic clocks.
+#[derive(Clone, Copy)]
+pub struct Instant<T: TimeNow> {
+    ns: u64,
+    _type: PhantomData<T>,
+}
+
+impl<T: TimeNow> Instant<T> {
+    fn new(ns: u64) -> Instant<T> {
+        Instant {
+            ns,
+            _type: PhantomData,
+        }
+    }
+
+    /// Returns the duration since this timestamp.
+    pub fn elasped(&self) -> Duration {
+        let now = T::now();
+
+        Duration::from_nanos(now.ns.saturating_sub(self.ns))
+    }
+
+    /// Returns the duration from another timestamp to this one.
+    pub fn duration_since(&self, earlier: Self) -> Duration {
+        Duration::from_nanos(self.ns.saturating_sub(earlier.ns))
+    }
+}
+
+/// CLOCK_MONOTONIC.
+#[derive(Clone, Copy)]
+pub struct ClockMonotonic;
+
+impl TimeNow for ClockMonotonic {
+    fn now() -> Instant<Self> {
+        // SAFETY: `ktime_get()` is always safe to call.
+        Instant::<Self>::new(unsafe { bindings::ktime_get() }.try_into().unwrap())
+    }
+}
+
+/// CLOCK_BOOTTIME.
+#[derive(Clone, Copy)]
+pub struct ClockBoottime;
+
+impl TimeNow for ClockBoottime {
+    fn now() -> Instant<Self> {
+        Instant::<Self>::new(
+            // SAFETY: `ktime_get_with_offset()` is always safe to call.
+            unsafe { bindings::ktime_get_with_offset(bindings::tk_offsets_TK_OFFS_BOOT) }
+                .try_into()
+                .unwrap(),
+        )
+    }
 }
